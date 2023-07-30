8daf9d1003e6f6b372b5cf07f0db1ed265d98095
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-07-30 13:20:27 +0530

    commit

--- src/SelectTeam.jsx
+++ src/SelectTeam.jsx
@@ -41,8 +41,9 @@ if(localStorage.getItem('token_for_first_time')!=null)
     return(
         <>
             <div>{console.warn(currentPath)}
-                <a href={currentPath+"/1"}><button className="h-2 w-2 p-2 m-2 border-2">1</button></a>
-                <a href={currentPath+"/2"}><button className="h-2 w-2 p-2 m-2 border-2">2</button></a>
+                <p className="text-lg">No of Team Members</p>
+                <a href={currentPath+"/1"}><button className=" p-5 m-4 border-2 text-lg">1</button></a>
+                <a href={currentPath+"/2"}><button className=" p-5 m-4 border-2 text-lg">2</button></a>
             </div>
         </>
     );
