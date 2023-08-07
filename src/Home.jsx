745a1e34e5a0e523359e380254322826d32fad0b
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-08-07 18:05:54 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,6 +17,10 @@ export default function Home(){
         <>
             <Loginnavbar />
 
+            <div className="loginbg">
+    <div className=' border p-4 bg-white bg-opacity-80 backdrop-filter p-6 rounded-lg shadow-lg'>
+            
+
             <p>If you are logging in for first time :</p><br></br>
             <li>Try any your mail as password and login.</li>
             <li>You will recieve otp to your mail. Enter and submit. Kindly check in your spam mails.</li>
@@ -39,7 +43,16 @@ export default function Home(){
             <li>If second member of a team want to access the dashboard, login with the first member mailid and password.</li>
             <br></br>
 
-            <button onClick={GoLogin} className="p-4 bg-red-700 text-white text-lg">Login</button>
+          <div className=' flex justify-center'>
+
+
+            <button onClick={GoLogin} className="bg-red-900 text-white px-6 py-2 rounded-md my-2 text-lg">Student Login</button>
+            </div>
+        
+            </div>
+            </div>
+
+
         <Footer />
         
         </>
