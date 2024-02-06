c0e0a42ebc01d7b36ac4e519c3c4efe38ed09304
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2024-02-06 13:23:56 +0530

    empty commit 3

--- src/Stafflogin.jsx
+++ src/Stafflogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
-import Loginnavbar from './shared/Loginnavbar';
+import Loginnavbar from './shared/LoginNavBar';
 import Footer from './shared/Footer';
 
 function LoadingScreen() {
