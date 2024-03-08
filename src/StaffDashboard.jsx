67de516c9345afe399780532cccea7ac47acb2a9
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2024-03-08 14:31:30 +0530

    commit

--- src/StaffDashboard.jsx
+++ src/StaffDashboard.jsx
@@ -175,7 +175,7 @@ const [guideImg, setGuideImg] = useState();
               </div>
               <div className="w-full flex items-center justify-center h-2/5 bg-slate-100 text-[#6C757D]  hover:bg-slate-200">
                 <a
-                  href={`/staff_dashboard/add_problem_statement`}
+                  href={`/staff_dashboard/change_password`}
                   className="w-full flex justify-start items-center gap-1 pl-2"
                 >
                   <img className="h-4 w-4" src={lock} alt="Lock" />
