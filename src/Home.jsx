95b93d4fcdd6260135135ae99dc9fa801bbdb731
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2024-05-01 12:18:16 +0530

    home page changed for showing project instructions for exam

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,6 +21,7 @@ export default function Home() {
 
         {/* <p className="font-bold text-red-600 text-center text-4xl p-0">Vacancies Have Been Updated Please Complete Your Registration</p> */}
 
+        <div className="hidden">
           <p className="font-semibold text-center text-2xl p-0">Registration Process for Students</p>
           <br></br>
           <li>Register with the mail id which is given to placement cell and password is your register number.</li>
@@ -60,14 +61,38 @@ export default function Home() {
             the first member mailid and password or with team id and password.
           </li>
           <br></br>
+          </div>
 
-          <div className=" flex justify-center">
-            <button
+
+          <div>
+
+          <p className="font-semibold text-center text-2xl p-0">Instructions For Students⚡⚡</p>
+
+          <br></br>
+
+         <p> 1. Chapter 1  - Introduction - Page number starts with 1 from this page onwards.</p>
+         <p> 2. Inform everyone to follow the same chapter headings as in Template given. (subdivisions can vary).</p>
+         <p> 3. Under Chapter 3, inside 3.1, they can include OBJECTIVE of the Project and its necessity.</p>
+         <p> 4. From the first page till List of Tables, the page numbers will be in Roman letters. From Chapter 1 onwards, digits based numbering style.</p>
+         <p> 5. Instruct the students to strictly adhere to the template.</p>
+
+          </div>
+          <br></br>
+
+          <div className=" lg:flex lg:flex-row justify-center space-x-3 flex-col space-y-2">
+            
+
+            <a href="https://drive.google.com/file/d/1LQzZmfyRaHpW9nJTVAXIVSpXXsa-Ztk3/view?usp=sharing" ><button className="bg-red-900 text-white px-6 py-2 mt-4 rounded-md my-2 text-lg">Project-Report Template</button></a>
+            <a href="https://docs.google.com/presentation/d/1cobAi_62TYNG96NgALy9NnjIifKzlaG2/edit?usp=sharing&ouid=113013420184703351472&rtpof=true&sd=true"><button className="bg-red-900 text-white px-6 py-2 rounded-md my-2 text-lg">PPT Template</button></a>
+            <a href="https://docs.google.com/spreadsheets/d/14aITm8U3IT1iApeSQRlPKDQIhE3xg2NM/edit?usp=sharing&ouid=113013420184703351472&rtpof=true&sd=true"><button className="bg-red-900 text-white px-6 py-2 rounded-md my-2 text-lg">Staff Mail Id's</button></a>
+            <a href="https://drive.google.com/file/d/1OiR_xbgeQHJhvG4ecPywEmccnnWHwzcI/view?usp=sharing" ><button className="bg-red-900 text-white px-6 py-2 rounded-md my-2 text-lg">Exam Shedule</button></a>
+
+            <a><button
               onClick={GoLogin}
               className="bg-red-900 text-white px-6 py-2 rounded-md my-2 text-lg"
             >
               Student Login
-            </button>
+            </button></a>
           </div>
         </div>
       </div>
@@ -76,3 +101,9 @@ export default function Home() {
     </>
   );
 }
+
+
+// ppt - https://docs.google.com/presentation/d/1cobAi_62TYNG96NgALy9NnjIifKzlaG2/edit?usp=sharing&ouid=113013420184703351472&rtpof=true&sd=true
+// staff mailids - https://docs.google.com/spreadsheets/d/14aITm8U3IT1iApeSQRlPKDQIhE3xg2NM/edit?usp=sharing&ouid=113013420184703351472&rtpof=true&sd=true
+// projectreport template -https://drive.google.com/file/d/1LQzZmfyRaHpW9nJTVAXIVSpXXsa-Ztk3/view?usp=sharing
+// shedule - https://drive.google.com/file/d/1OiR_xbgeQHJhvG4ecPywEmccnnWHwzcI/view?usp=sharing
